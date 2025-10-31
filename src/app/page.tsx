f0fae6560a31796f9c341d78e853f0e691f60189
Fabien VITRAT <99119582+fabien-vitrat@users.noreply.github.com>
2025-10-31 11:58:02 +0100

    Added portfolio.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import About from "@/components/about";
 import Values from "@/components/values";
 import Goals from "@/components/goals";
 import Course from "@/components/course";
+import Portfolio from "@/components/portfolio";
 import Navigation from "@/components/navigation";
 
 export default function Home() {
@@ -30,6 +31,7 @@ export default function Home() {
         <Values />
         <Goals />
         <Course />
+        <Portfolio />
       </main>
     </>
   );
